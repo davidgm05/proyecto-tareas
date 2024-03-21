8fc8747796ab1965b6d0d5741d69ba00a1a1b427
davidgm05 <davidgutierrezmillan05@gmail.com>
2024-03-21 11:59:00 +0100

    commit jueves 11 58

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,40 @@
 import '../scss/style.scss';
 const taskImput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
-const taskList = document.getElementById("taskList");
+const pendingTasks = document.getElementById("pending-tasks");
+const completedTasks = document.getElementById("completed-tasks");
 
 addTaskBtn.addEventListener('click', addTask);
+
 
 function addTask(){
     const taskValue = taskImput.value;
     if(taskValue !== ""){
-        const li = document.createElement("li");
-        li.classList.add("li");
-        li.textContent = taskValue;
-        
-        const buttomTask = document.createElement("buttom");
-        buttomTask.textContent = "tarea completada";
-        buttomTask.classList.add("buttom-task");
-        
-        taskList.appendChild(li);
-        li.appendChild(buttomTask)
+        const liPending = document.createElement("li");
+        liPending.textContent = taskValue;
 
-        buttomTask.addEventListener("click", () => {
-            li.remove();
-        })
-    
+        // Agregar botón de completado
+        const completeButton = document.createElement("button");
+        completeButton.textContent = "Completado";
+        completeButton.classList.add("buttom-task");
+        liPending.appendChild(completeButton);
+
+        // Agregar tarea a la lista de tareas pendientes
+        pendingTasks.appendChild(liPending);
+
+        // Limpiar el campo de entrada después de agregar la tarea
+        taskInput.value = "";
+
+        completeButton.addEventListener('click', TaskComplete);
+
+        function TaskComplete(){
+            const liCompleted = document.createElement("li");
+            liCompleted.textContent = taskValue;
+            liPending.remove();
+            completedTasks.appendChild(liCompleted);
+        }
     }
 }
+
 
 
